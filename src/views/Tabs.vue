fdc0e2adb19ded6ac466a59219717d7fbebf141e
Natnipa S <s.natnipa@gmail.com>
2021-01-10 13:03:29 +0700

    update icons

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,27 +3,27 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="้swipe-shop" href="/swipe-shop">
-          <ion-icon :icon="triangle" />
+          <ion-icon :icon="dice" />
           <ion-label>Swipe&Shop</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="contents" href="/contents">
-          <ion-icon :icon="ellipse" />
+          <ion-icon :icon="document" />
           <ion-label>บทความ</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="้home" href="/home">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="home"/>
           <ion-label>หน้าแรก</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="cart" href="/cart">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="cart" />
           <ion-label>ตะกร้าสินค้า</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="account" href="/account">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="person" />
           <ion-label>บัญชี</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -33,16 +33,18 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { home, cart, person, dice, document } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      home,
+      cart,
+      person,
+      dice,
+      document,
     }
   }
 }
