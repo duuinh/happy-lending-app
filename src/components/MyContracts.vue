3c365728a1f34b0d99aa69bb871f0013db1691c6
Natnipa S <s.natnipa@gmail.com>
2023-04-03 19:43:23 +0200

    [implement] end contract

--- src/components/MyContracts.vue
+++ src/components/MyContracts.vue
@@ -2,104 +2,119 @@
   <div class="q-pa-md full-width">
     <q-list padding>
       <q-item-label header>Lending Items</q-item-label>
-      <template
-        v-for="contract in contractStore.lendingItems"
-        :key="contract._id"
-      >
-        <q-item>
-          <q-item-section top thumbnail class="q-ml-none">
-            <q-avatar size="100px">
-              <img :src="contract.item.img_url"
-            /></q-avatar>
-          </q-item-section>
+      <div v-if="contractStore.acceptedLendingContracts.length">
+        <template
+          v-for="contract in contractStore.acceptedLendingContracts"
+          :key="contract._id"
+        >
+          <q-item>
+            <q-item-section top thumbnail class="q-ml-none">
+              <q-avatar size="100px">
+                <img :src="contract.item.img_url"
+              /></q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label>{{ contract.item.name }}</q-item-label>
-            <q-space />
-            <q-item-label caption>
-              <div class="q-py-xs">
-                <b>Borrowed by:</b> {{ contract.borrower.name }}
-              </div>
-              <div class="text-weight-bold">Pick-up date:</div>
-              <div class="q-pb-xs">
-                {{
-                  dayjs(contract.pick_up_date).format(
-                    "MMM DD, YYYY (ddd) - hh:mm A"
-                  )
-                }}
-              </div>
-              <div class="text-weight-bold">Return date:</div>
-              <div>
-                {{
-                  dayjs(contract.return_date).format(
-                    "MMM DD, YYYY (ddd) - hh:mm A"
-                  )
-                }}
-              </div>
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top>
-            <q-item-label caption>{{
-              dayjs(contract.updatedAt).fromNow()
-            }}</q-item-label>
-            <q-btn color="deep-orange-5" class="q-mt-md">
-              <div class="row items-center no-wrap">
-                <div class="text-center">
-                  <q-icon name="task_alt" /> <br />End
+            <q-item-section>
+              <q-item-label>{{ contract.item.name }}</q-item-label>
+              <q-space />
+              <q-item-label caption>
+                <div class="q-py-xs">
+                  <b>Borrower:</b> {{ contract.borrower.name }} (📞{{
+                    contract.borrower.phone_no
+                  }})
                 </div>
-              </div>
-            </q-btn>
-          </q-item-section>
-        </q-item>
-        <q-separator spaced />
-      </template>
+                <div class="text-weight-bold">Pick-up date:</div>
+                <div class="q-pb-xs">
+                  {{
+                    dayjs(contract.pick_up_date).format(
+                      "MMM DD, YYYY (ddd) - hh:mm A"
+                    )
+                  }}
+                </div>
+                <div class="text-weight-bold">Return date:</div>
+                <div>
+                  {{
+                    dayjs(contract.return_date).format(
+                      "MMM DD, YYYY (ddd) - hh:mm A"
+                    )
+                  }}
+                </div>
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side top>
+              <q-item-label caption>{{
+                dayjs(contract.updatedAt).fromNow()
+              }}</q-item-label>
+              <q-btn
+                color="deep-orange-5"
+                class="q-mt-md"
+                @click="endContract(contract._id)"
+              >
+                <div class="row items-center no-wrap">
+                  <div class="text-center">
+                    <q-icon name="task_alt" /> <br />End
+                  </div>
+                </div>
+              </q-btn>
+            </q-item-section>
+          </q-item>
+          <q-separator spaced />
+        </template>
+      </div>
+      <div v-else class="flex flex-center text-grey">No Lending Items</div>
       <q-item-label header>Borrowing Items</q-item-label>
-      <template
-        v-for="contract in contractStore.borrowingItems"
-        :key="contract._id"
-      >
-        <q-item>
-          <q-item-section top thumbnail class="q-ml-none">
-            <q-avatar size="100px">
-              <img :src="contract.item.img_url" />
-            </q-avatar>
-          </q-item-section>
+      <div v-if="contractStore.borrowingContracts.length">
+        <template
+          v-for="contract in contractStore.borrowingContracts"
+          :key="contract._id"
+        >
+          <q-item>
+            <q-item-section top thumbnail class="q-ml-none">
+              <q-avatar size="100px">
+                <img :src="contract.item.img_url" />
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label>{{ contract.item.name }}</q-item-label>
-            <q-space />
-            <q-item-label caption>
-              <div class="q-py-xs">
-                <b>Borrowing from:</b> {{ contract.lender.name }}
-              </div>
-              <div class="text-weight-bold">Pick-up date:</div>
-              <div class="q-pb-xs">
-                {{
-                  dayjs(contract.pick_up_date).format(
-                    "MMM DD, YYYY (ddd) - hh:mm A"
-                  )
-                }}
-              </div>
-              <div class="text-weight-bold">Return date:</div>
-              <div>
-                {{
-                  dayjs(contract.return_date).format(
-                    "MMM DD, YYYY (ddd) - hh:mm A"
-                  )
-                }}
-              </div>
-            </q-item-label>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ contract.item.name }}</q-item-label>
+              <q-space />
+              <q-item-label caption>
+                <div class="q-py-xs">
+                  <b>Owner:</b> {{ contract.lender.name }} (📞{{
+                    contract.lender.phone_no
+                  }})
+                </div>
+                <div class="q-py-xs"><b>Status:</b> {{ contract.status }}</div>
+                <div class="text-weight-bold">Pick-up date:</div>
+                <div class="q-pb-xs">
+                  {{
+                    dayjs(contract.pick_up_date).format(
+                      "MMM DD, YYYY (ddd) - hh:mm A"
+                    )
+                  }}
+                </div>
+                <div class="text-weight-bold">Return date:</div>
+                <div>
+                  {{
+                    dayjs(contract.return_date).format(
+                      "MMM DD, YYYY (ddd) - hh:mm A"
+                    )
+                  }}
+                </div>
+              </q-item-label>
+            </q-item-section>
 
-          <q-item-section side top>
-            <q-item-label caption>{{
-              dayjs(contract.updatedAt).fromNow()
-            }}</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-separator spaced />
-      </template>
+            <q-item-section side top>
+              <q-item-label caption>{{
+                dayjs(contract.updatedAt).fromNow()
+              }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator spaced />
+        </template>
+      </div>
+      <div v-else class="flex flex-center text-grey">No Borrowing Items</div>
     </q-list>
   </div>
 </template>
@@ -110,6 +125,7 @@ import { useQuasar } from "quasar";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { useContractStore } from "src/stores/contract";
+import { ContractStatusEnum } from "src/enums";
 
 export default defineComponent({
   name: "MyContracts",
@@ -124,7 +140,14 @@ export default defineComponent({
       await contractStore.fetchBorrowingItems();
       $q.loading.hide();
     });
-    return { contractStore, dayjs };
+
+    async function endContract(id) {
+      $q.loading.show();
+      await contractStore.updateContractStatus(id, ContractStatusEnum.closed);
+      await contractStore.fetchLendingItems();
+      $q.loading.hide();
+    }
+    return { contractStore, dayjs, endContract };
   },
 });
 </script>
